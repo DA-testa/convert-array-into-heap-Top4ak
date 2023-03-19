d95c5d8802efcdee48b79f095db2b683de0f073d
Top4ak <120311720+Top4ak@users.noreply.github.com>
2023-03-19 23:53:53 +0200

    221RDB014

--- build_heap.py
+++ build_heap.py
@@ -1,39 +1,60 @@
-# python3
+# 221RDB014 Mihails Ruhļa 13. grupa
 
-
-def build_heap(data):
+def build_heap(arr):
+    n = len(arr)
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
+    
+    for i in range(n//2 - 1, -1, -1):
+        heap(arr, n, i, swaps)
+    
     return swaps
 
+def heap(arr):
+    n = len(arr)
+    swaps = []
+    
+    for i in range(n):
+        min_index = i
+        left_index = 2 * i + 1
+        right_index = 2 * i + 2
+        if left_index < n and arr[left_index] < arr[min_index]:
+            min_index = left_index
+        if right_index < n and arr[right_index] < arr[min_index]:
+            min_index = right_index
+        if min_index != i:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+            swaps.append((i, min_index))
+            i = min_index
+            
+    return swaps
 
 def main():
     
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
+    let= input().strip()
+    if let == 'I':
+        n = int(input().strip())
+        data = list(map(int, input().split()))
+        assert len(data) == n
+    elif let == 'F':
+        file = input().strip()
+        file = "tests/" + file
+        with open(file, "r") as f:
+            n = int(f.readline())
+            data = list(map(int, f.readline().split()))  
+            
+            assert len(data) == n
+            
     # calls function to assess the data 
     # and give back all swaps
     swaps = build_heap(data)
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
+    assert len(swaps) <= 4 * n
     for i, j in swaps:
         print(i, j)
 
